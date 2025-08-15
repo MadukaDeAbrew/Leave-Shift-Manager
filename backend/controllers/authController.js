c887045c03e65adc36ed983ae41d2fefd52ee183
Maduka De Abrew <deabrewmaduka@gmail.com>
2025-08-16 02:41:14 +1000

    LSMA-35 2.5: Redirect to shift page & role based redirection

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -50,7 +50,7 @@ const registerUser = async (req, res) => {
       id: user.id,
       name: user.name,
       email: user.email,
-      role: user.role,
+      role: user.role, //2.5 role saved
       token: generateToken(user.id),
     });
   } catch (e) {
