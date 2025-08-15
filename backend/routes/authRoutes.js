945d63191b4dd159440c329694b6b4e691ead28f
Maduka De Abrew <deabrewmaduka@gmail.com>
2025-08-15 20:34:07 +1000

    LSMA-28 1.3: Connect to backend /api/signup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+
 const { registerUser, loginUser, updateUserProfile, getProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
