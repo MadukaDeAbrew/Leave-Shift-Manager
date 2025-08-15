a929156939f796977c833200eaf9b93be021566d
Maduka De Abrew <deabrewmaduka@gmail.com>
2025-08-16 01:27:36 +1000

    LSMA-33 	2.3: Connection to backend /api/login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,8 +5,8 @@ const { registerUser, loginUser, updateUserProfile, getProfile } = require('../c
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', registerUser); //1.3
+router.post('/login', loginUser); //2.3
 router.get('/profile', protect, getProfile);
 router.put('/profile', protect, updateUserProfile);
 
