17425ec7628ea877eac383d440b237d4c7d8ae8d
Maduka De Abrew <deabrewmaduka@gmail.com>
2025-08-28 12:26:26 +1000

    prod ip updated

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -14,7 +14,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // REPLACE <YOUR_EC2_IP> with 3.27.187.70 (keep http and :5001)
-  baseURL: 'http://3.27.187.70:5001',
+  baseURL: 'http://3.107.58.27:5001',
   headers: { 'Content-Type': 'application/json' },
 });
 
