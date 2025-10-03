a0b52e02b96c281eda4a00b7707dbdcd6f475741
Maduka De Abrew <deabrewmaduka@gmail.com>
2025-10-03 17:57:44 +1000

    YML file, .env prod url updated

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -14,7 +14,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.54.210.184:5001', // live
+  baseURL: 'http://13.211.239.207:5001', // live
 
   headers: { 'Content-Type': 'application/json' },
 });
